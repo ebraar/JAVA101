dd28923ed748dcc8c6c4fa64a9ffb15a29862879
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-09-30 16:36:14 +0300

    Add files via upload

---
+++ NotHesaplama.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class NotHesaplama {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz: ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz: ");
+        kimya = inp.nextInt();
+
+        System.out.print("Turce Notunuz: ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz: ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6;
+
+        System.out.print("Not Ortalaman: ");
+        System.out.println(sonuc);
+
+        double a;
+        System.out.println(a=(sonuc>=60)?1:0);
+    }
+}
