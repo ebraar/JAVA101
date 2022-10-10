741e2dfe113d92c233f82b819f72b15eac1f32b7
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-10-10 23:26:36 +0300

    Add files via upload

---
+++ CinZodyagi.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class CinZodyagi{
+    public static void main(String[] args) {
+        int yil,kalan;
+        String burc="";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum yılınızı giriniz: ");
+        yil = input.nextInt();
+
+        kalan = yil%12;
+
+        switch (kalan) {
+            case 0:
+                burc = "Maymun";
+                break;
+            case 1:
+                burc = "Horoz";
+                break;
+            case 2:
+                burc = "Kopek";
+                break;
+            case 3:
+                burc = "Domuz";
+                break;
+            case 4:
+                burc = "Fare";
+                break;
+            case 5:
+                burc = "Okuz";
+                break;
+            case 6:
+                burc = "Kaplan";
+                break;
+            case 7:
+                burc = "Tavsan";
+                break;
+            case 8:
+                burc = "Ejderha";
+                break;
+            case 9:
+                burc = "Yilan";
+                break;
+            case 10:
+                burc = "At";
+                break;
+            case 11:
+                burc = "Koyun";
+                break;
+
+        }
+        System.out.println("Çin Zodyağı Burcunuz: "+burc);
+    }
+}
