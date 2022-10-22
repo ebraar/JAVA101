b5cfc6aff0cdd9c3b26a7dcbdda5ca7f6f698e5c
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-10-22 21:21:11 +0300

    Add files via upload

---
+++ DeseneGoreMetotOlusturma.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class DeseneGoreMetotOlusturma {
+
+    static int negative(int x) {
+        if (x <= 0){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return negative(x-5);
+        }
+    }
+
+    static int positive(int x,int y) {
+        if(x > y){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return positive(x+5,y);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.print("sayıyı giriniz: ");
+            int number = input.nextInt();
+
+            positive(negative(number),number);
+
+            if (number == 0){
+                break;
+            }
+
+            System.out.println("\nCikmak icin 0'i giriniz.");
+        }
+    }
+}
+
+
