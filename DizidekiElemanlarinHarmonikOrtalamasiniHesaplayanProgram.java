af7a1d8ee2969ca199edc856540b53dc489d5008
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-11-02 15:20:21 +0300

    Add files via upload

---
+++ DizidekiElemanlarinHarmonikOrtalamasiniHesaplayanProgram.java
@@ -0,0 +1,13 @@
+public class DizidekiElemanlarinHarmonikOrtalamasiniHesaplayanProgram {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        double total;
+        float result = 0;
+        for(int j: array){
+            result += 1f / j;
+        }
+        total = array.length / result;
+
+        System.out.println("Harmonic Average = "+total);
+    }
+}
