32c22138bc462388a799bedc4ad552c7ac164f5b
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-11-03 18:12:26 +0300

    Add files via upload

---
+++ DizilerleYildiz.java
@@ -0,0 +1,24 @@
+public class DizilerleYildiz {
+    public static void main(String[] args) {
+
+        String[][] letter = new String[7][4];
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = "*  ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = "*  ";
+                } else {
+                    letter[i][j] = "    ";
+                }
+            }
+
+        }
+        for (String row[] : letter) {
+            for (String Col : row) {
+                System.out.print(Col);
+            }
+            System.out.println();
+        }
+    }
+}
